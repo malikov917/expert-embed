75f07dd46ad6e5e70f283fee8a5a1857ab4a70e3
Kanstantsin Malikau <kanstanstin.malikau@haiilo.com>
2025-09-28 21:44:52 +0200

    lint fixes

--- src/components/ForumPage.jsx
+++ src/components/ForumPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from './Header';
 import PartnerUpWidget from './PartnerUpWidget';
 import { MessageSquare, Send, Hash, Rss } from 'lucide-react';
